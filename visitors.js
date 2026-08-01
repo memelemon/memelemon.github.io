ba52d0ec41e0d3d9ad996f81b4fa03be182046da
meme_lemon <escapemmlmn@gmail.com>
2026-08-01 22:57:13 +0300

    Update visitors.js

--- visitors.js
+++ visitors.js
@@ -1,5 +1,5 @@
 fetch("https://memelemoncounter.escapemmlmn.workers.dev")
   .then(response => response.json())
   .then(data => {
-    document.getElementById("visitors").innerText = data.visitors;
+    document.getElementById("visitorCount").innerText = data.visitors;
   });
